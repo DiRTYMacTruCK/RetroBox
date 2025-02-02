98cfffc6da53973bcf8a80ac3795b8ed5dfa26c2
DiRTY <velez.michael.n@gmail.com>
2025-02-01 22:20:51 -0700

    dirty

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,8 @@
 #include <QDir>
 #include <QMediaPlayer>
 #include <QMediaPlaylist>
+#include <QDirIterator>
+#include <QMediaMetaData>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,10 +19,18 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     mediaPlayer->setPlaylist(playlist);
 
+    // Ensure mediaPlayer runs on the main thread
+    mediaPlayer->moveToThread(QApplication::instance()->thread());
+
     connect(ui->actionExit, &QAction::triggered, this, &MainWindow::on_actionExit_triggered);
-    connect(ui->actionOpen_Library, &QAction::triggered, this, &MainWindow::on_actionOpenLibrary_triggered);
+    connect(ui->actionOpenLibrary, &QAction::triggered, this, &MainWindow::on_actionOpenLibrary_triggered);
     connect(ui->playButton, &QPushButton::clicked, this, &MainWindow::on_playButton_clicked);
+    connect(ui->listWidget, &QListWidget::itemClicked, this, &MainWindow::on_songSelected);
+
+    // Correct signal connection for Qt 6 (Updated method)
+    connect(mediaPlayer, &QMediaPlayer::mediaStatusChanged, this, &MainWindow::on_mediaStatusChanged);
 }
+
 MainWindow::~MainWindow()
 {
     delete ui;
@@ -30,6 +40,7 @@ void MainWindow::on_actionExit_triggered()
 {
     QApplication::quit();
 }
+
 void MainWindow::openSettings()
 {
     SettingsDialog settingsDialog(this);
@@ -38,20 +49,129 @@ void MainWindow::openSettings()
 
 void MainWindow::on_actionOpenLibrary_triggered()
 {
+    // Open file dialog ONCE
     QString directoryPath = QFileDialog::getExistingDirectory(this, tr("Open Library"), "",
                                                               QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
 
-    if (!directoryPath.isEmpty()) {
-        // Scan the directory for audio files
-        QDir directory(directoryPath);
-        QStringList filters;
-        filters << "*.mp3" << "*.wav" << "*.flac" << "*.ogg";  // Add more audio file formats as needed
-        QStringList audioFiles = directory.entryList(filters, QDir::Files | QDir::Readable, QDir::Name);
+    if (directoryPath.isEmpty()) return; // Exit if no folder is selected
 
-        // Clear any previous list and add new files
-        ui->listWidget->clear();
-        for (const QString &file : audioFiles) {
-            ui->listWidget->addItem(file);
-        }
+    ui->listWidget->clear();  // Clear previous list
+
+    QDirIterator it(directoryPath, {"*.mp3", "*.wav", "*.flac", "*.ogg"},
+                    QDir::Files, QDirIterator::Subdirectories);
+
+    while (it.hasNext()) {
+        QString filePath = it.next();
+
+        QMediaPlayer tempPlayer;
+        tempPlayer.setMedia(QUrl::fromLocalFile(filePath));
+
+        // Wait for metadata to load
+        QEventLoop loop;
+        connect(&tempPlayer, &QMediaPlayer::mediaStatusChanged, &loop, &QEventLoop::quit);
+        tempPlayer.play();
+        loop.exec();
+
+        QString title = tempPlayer.metaData(QMediaMetaData::Title).toString();
+        QString artist = tempPlayer.metaData(QMediaMetaData::Author).toString();
+        QString album = tempPlayer.metaData(QMediaMetaData::AlbumTitle).toString();
+
+        if (title.isEmpty()) title = QFileInfo(filePath).fileName();  // Use filename if no metadata
+
+        QString displayText = QString("%1 - %2 (%3)")
+                                  .arg(artist.isEmpty() ? "Unknown Artist" : artist)
+                                  .arg(title)
+                                  .arg(album.isEmpty() ? "Unknown Album" : album);
+
+        QListWidgetItem *item = new QListWidgetItem(displayText);
+        item->setData(Qt::UserRole, filePath);  // Store file path
+        ui->listWidget->addItem(item);
+    }
+}
+
+void MainWindow::on_playButton_clicked()
+{
+    if (mediaPlayer->state() == QMediaPlayer::PlayingState) {
+        mediaPlayer->pause();
+    } else {
+        mediaPlayer->play();
+    }
+}
+
+void MainWindow::on_songSelected(QListWidgetItem *item)
+{
+    QString filePath = item->data(Qt::UserRole).toString();
+    mediaPlayer->setMedia(QUrl::fromLocalFile(filePath));
+
+    // Retrieve the metadata after the media is loaded
+    QString title = mediaPlayer->metaData(QMediaMetaData::Title).toString();
+    QString artist = mediaPlayer->metaData(QMediaMetaData::Author).toString();
+    QString album = mediaPlayer->metaData(QMediaMetaData::AlbumTitle).toString();
+
+    QString metadataText = QString("Title: %1\nArtist: %2\nAlbum: %3")
+                               .arg(title.isEmpty() ? "Unknown Title" : title)
+                               .arg(artist.isEmpty() ? "Unknown Artist" : artist)
+                               .arg(album.isEmpty() ? "Unknown Album" : album);
+
+    QMessageBox::information(this, "Song Metadata", metadataText);
+}
+
+void MainWindow::fetchMetadata(const QString &filePath, QString &title, QString &artist, QString &album)
+{
+    QMediaPlayer tempPlayer;
+    tempPlayer.setMedia(QUrl::fromLocalFile(filePath));
+
+    // Wait for metadata to load
+    QEventLoop loop;
+    connect(&tempPlayer, &QMediaPlayer::mediaStatusChanged, &loop, &QEventLoop::quit);
+    tempPlayer.play();
+    loop.exec();
+
+    title = tempPlayer.metaData(QMediaMetaData::Title).toString();
+    artist = tempPlayer.metaData(QMediaMetaData::Author).toString();
+    album = tempPlayer.metaData(QMediaMetaData::AlbumTitle).toString();
+
+    if (title.isEmpty()) title = QFileInfo(filePath).fileName();  // Use filename if no metadata
+}
+
+void MainWindow::on_metaDataChanged()
+{
+    if (mediaPlayer->isMetaDataAvailable()) {
+        QVariant titleVariant = mediaPlayer->metaData(QMediaMetaData::Title);
+        QString title = titleVariant.isValid() ? titleVariant.toString() : "Unknown Title";
+
+        QVariant artistVariant = mediaPlayer->metaData(QMediaMetaData::Author);
+        QString artist = artistVariant.isValid() ? artistVariant.toString() : "Unknown Artist";
+
+        ui->labelTitle->setText(title + " - " + artist);
+    }
+}
+
+void MainWindow::on_mediaStatusChanged(QMediaPlayer::MediaStatus status)
+{
+    switch (status) {
+    case QMediaPlayer::NoMedia:
+        qDebug() << "No media loaded.";
+        break;
+    case QMediaPlayer::LoadingMedia:
+        qDebug() << "Loading media...";
+        break;
+    case QMediaPlayer::LoadedMedia:
+        qDebug() << "Media loaded.";
+        break;
+    case QMediaPlayer::BufferingMedia:
+        qDebug() << "Buffering...";
+        break;
+    case QMediaPlayer::StalledMedia:
+        qDebug() << "Playback stalled.";
+        break;
+    case QMediaPlayer::EndOfMedia:
+        qDebug() << "End of media reached.";
+        break;
+    case QMediaPlayer::InvalidMedia:
+        qDebug() << "Invalid media.";
+        break;
+    default:
+        break;
     }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,24 +4,29 @@
 #include <QMainWindow>
 #include <QMediaPlayer>
 #include <QMediaPlaylist>
+#include <QListWidgetItem>
 
-namespace Ui {
-class MainWindow;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
-    void on_actionOpenLibrary_triggered();
     void on_actionExit_triggered();
-    void openSettings();
+    void on_actionOpenLibrary_triggered();
     void on_playButton_clicked();
+    void on_songSelected(QListWidgetItem *item);
+    void on_mediaStatusChanged(QMediaPlayer::MediaStatus status);
+    void openSettings();
+    void on_metaDataChanged();
+    void fetchMetadata(const QString &filePath, QString &title, QString &artist, QString &album);
 
 private:
     Ui::MainWindow *ui;
@@ -29,4 +34,4 @@ private:
     QMediaPlaylist *playlist;
 };
 
-#endif
+#endif // MAINWINDOW_H
